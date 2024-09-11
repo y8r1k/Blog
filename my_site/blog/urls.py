5f4de166a5b2a39146922267ce94c4c6f530a1de
ygrik <panchenko.igorr@yandex.ru>
2024-09-11 10:08:48 +0300

    Adding a class-based PostListView

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -4,7 +4,6 @@ from blog import views
 app_name = 'blog'
 
 urlpatterns = [
-    # представления поста
-    path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
 ]
--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,26 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 from blog.models import Post
 
-# Create your views here.
-def post_list(request):
-    post_list = Post.published.all()
-    # Постраничкая разбивка по 3 поста на страницу
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        # Если page_number не целое число, то
-        # выдать первую страницу
-        posts = paginator.page(1)
-    except EmptyPage:
-        # Если page_number находится вне диапазона, то
-        # выдать последнюю страницу
-        posts = paginator.page(paginator.num_pages)
-    return render(request,
-                  'blog/post/list.html',
-                  {'posts':posts})
+class PostListView(ListView):
+    """
+    Альтернативное представление списка постов
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
