aa3b6b6ce87765ec9529079fc833020b1193cbe3
ygrik <panchenko.igorr@yandex.ru>
2024-09-15 14:14:13 +0300

    adding an email recommendation mechanism for posts

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from blog.models import Post
+from blog.form import EmailPostForm
+from django.core.mail import send_mail
 
 class PostListView(ListView):
     """
@@ -20,3 +22,27 @@ def post_detail(request, year, month, day, post):
                             publish__month=month,
                             publish__day=day)
     return render(request, 'blog/post/detail.html', {'post':post})
+
+def post_share(request, post_id):
+    # Извлечь пост по идентификатору  id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+
+    sent = False
+
+    if request.method == 'POST':
+        # Форма была передана в обработку
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Поля формы успешно прошли валидацию
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n"\
+                      f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'your_account@gmail.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
